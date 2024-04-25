660f1d47b294a65e88bf690d8d7eccafc42df858
muhammad aji solehudin <muhammadajisolehudin@gmail.com>
2024-04-25 20:22:29 +0700

    challange 1

---
+++ dummy.js
@@ -0,0 +1,39 @@
+function printData() {
+    console.log("Data from printData function.");
+}
+
+function printDataCustom(message) {
+    console.log(`Custom message from printDataCustom: ${message}`);
+}
+
+//kubus 
+function volumeKubus(s) {
+    return s * s * s;
+}
+
+function luasKubus(s) {
+    return 6 * s * s;
+}
+
+//balok 
+function volumeBalok(length, width, height) {
+    return length * width * height;
+}
+
+function luasBalok(length, width, height) {
+    return 2 * (length * width + length * height + width * height);
+}
+
+//prisma
+function volumeprisma(base, triangleHeight, prismHeight) {
+    return 0.5 * base * triangleHeight * prismHeight;
+}
+
+function luasPrisma(base, triangleHeight, prismHeight) {
+    let baseArea = 0.5 * base * triangleHeight;
+    let lateralArea = 3 * base * prismHeight; 
+    return 2 * baseArea + lateralArea;
+}
+
+
+module.exports = { printData, printDataCustom, volumeKubus, luasKubus, volumeBalok, luasBalok, volumeprisma, luasPrisma };
---
+++ index.js
@@ -0,0 +1,30 @@
+const { printData, volumeKubus, luasKubus, volumeBalok, luasBalok, volumeprisma, luasPrisma } =require('./dummy');
+
+printData();
+
+// custom modul, luas dan volume 
+// kubus
+let sisiK = 5;  //  sisi kubus 
+
+let vKubus = volumeKubus(sisiK);
+let lKubus = luasKubus(sisiK);
+
+
+// balok
+let panjangB = 10; 
+let lebarB= 5;    
+let tinggiB = 3;   
+
+let vBalok = volumeBalok(panjangB, lebarB, tinggiB);
+let lBalok = luasBalok(panjangB, lebarB, tinggiB);
+
+// prisma segitiga 
+let alas = 6; 
+let tinggiS = 4; // Tinggi segitiga 4
+let tinggiP = 10; // Tinggi prisma 10
+
+let vPrisma = volumeprisma(alas, tinggiS, tinggiP);
+let lPrisma = luasPrisma(alas, tinggiS, tinggiP);
+
+console.log("luas balok :", lBalok, "kubus :", lKubus, "prisma :", lPrisma);
+console.log("volumw balok :", vBalok, "kubus :", vKubus, "prisma :", vPrisma);
